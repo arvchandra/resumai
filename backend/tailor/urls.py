b92ba6c3a24a27cc435931fadab078dcb2b63e41
Arvind Chandra <arvchandra@gmail.com>
2025-06-30 19:23:30 +0530

    Created Tailor Resume API endpoint with basic validation and simple response

--- backend/tailor/urls.py
+++ backend/tailor/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from tailor.api.views import (
     UserResumeUploadView, 
     ParseJobPostingView, 
+    TailorResumeView,
     UserResumeListView
 )
 
@@ -20,6 +21,10 @@ urlpatterns = [
         "users/<int:user_id>/resume/upload",
         UserResumeUploadView.as_view(),
         name="user-resume-upload",
-    )
-
+    ),
+    path(
+        "users/<int:user_id>/tailor-resume",
+        TailorResumeView.as_view(),
+        name="tailor-resume",
+    ),
 ]
