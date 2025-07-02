5df183362e70704ebf08de5ee44aae6a297fecf1
Max Ingraham-Rakatansky <maxmir@berkeley.edu>
2025-07-02 10:40:01 +0100

    fix class name for docx

--- backend/tailor/domain/document/docx.py
+++ backend/tailor/domain/document/docx.py
@@ -3,7 +3,7 @@ import docx
 
 
 class DocxDocument(Document):
-    def parse_text(self):
+    def parse_file_text(self):
         doc = docx.Document(self.file_path)
         for paragraph in doc.paragraphs:
             print(paragraph.text)
