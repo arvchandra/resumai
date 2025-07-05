b9aab29733409d5a08cf146be129be951e0e4874
Arvind Chandra <arvchandra@gmail.com>
2025-07-05 18:06:14 +0530

    Renamed "file" context variable to "f" to avoid confusion with self.file

--- backend/tailor/domain/document/txt.py
+++ backend/tailor/domain/document/txt.py
@@ -3,8 +3,8 @@ from .base import Document
 
 class TxtDocument(Document):
     def get_text(self):
-        with open(self.file.path, 'rb') as file:
-            return file.read()
+        with open(self.file.path, 'rb') as f:
+            return f.read()
 
     def generate_copy(self):
         pass
