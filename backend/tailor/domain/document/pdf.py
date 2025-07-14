9a6fa47cc12b97ec848ee4a55490068d55b1da3e
Max Ingraham-Rakatansky <maxmir@berkeley.edu>
2025-07-14 19:43:15 +0100

    refactored

--- backend/tailor/domain/document/pdf.py
+++ backend/tailor/domain/document/pdf.py
@@ -2,8 +2,6 @@ import shutil
 
 import pymupdf4llm
 import pymupdf
-from shutil import copy
-from django.conf import settings
 import os
 
 from .base import Document
@@ -26,19 +24,19 @@ class PdfDocument(Document):
     def __init__(self, file):
         super().__init__(file)
         self.source_pdf_details = {
-            "page_count": None,
-            "width": None,
-            "height": None
+            "page_count": 0,
+            "width": 0,
+            "height": 0
         }
         self.redacted_rects = []
-        self.header_breaks = {}
+        self.page_break_rects = []
 
     def get_text(self):
         return pymupdf4llm.to_markdown(self.file.path)
 
     def generate_copy(self, bullets_to_redact=HARDCODED_BULLETS):
         # Clone PDF of file and save in examples
-        # TODO load into a temp memory file
+        # TODO replace with Resume fetch
         try:
             os.remove(DESTINATION_DIR_PATH)
             print("file already exists, deleting")
@@ -49,40 +47,33 @@ class PdfDocument(Document):
         copied_path = shutil.copy2(HARDCODED_RESUME_PATH, DESTINATION_DIR_PATH)
 
         # TODO better to create the single page pdf and identify bullets/header spacing when first uploaded
-        source_pdf_unified, tailored_pdf_unified = self.generate_unified_pdfs(copied_path)
-        source_page_unified, tailored_page_unified = source_pdf_unified[0], tailored_pdf_unified[0]
-
-        redacted_source_page = self.redact_bullets_from_page(bullets_to_redact, source_page_unified)
-
         try:
-            self.format_tailored_pdf_unified(
-                redacted_source_page,
-                tailored_page_unified,
-                source_pdf_unified
-            )
+            source_pdf_unified = self.generate_unified_pdf(copied_path)
+            redacted_pdf_unified = self.redact_bullets_from_pdf(bullets_to_redact, source_pdf_unified)
+            tailored_pdf_unified = self.format_tailored_pdf_unified(redacted_pdf_unified)
 
             tailored_resume = self.split_unified_pdf(tailored_pdf_unified)
-            tailored_resume.save("tailored_resume.pdf")
 
+            # TODO remove after testing
             source_pdf_unified.save("unified.pdf")
             tailored_pdf_unified.save("unified_tailored.pdf")
 
-            # Can annotate and delete bullet point
-
-            # Page with deleted bullet point corrects page format
+            tailored_resume.save("tailored_resume.pdf")
         except Exception as e:
             print(f"error: {e}")
             return
         finally:
             source_pdf_unified.close()
             tailored_pdf_unified.close()
+            tailored_resume.close()
 
-    def generate_unified_pdfs(self, path_to_source_pdf):
+    def generate_unified_pdf(self, path_to_source_pdf):
         """
-        This function will take a source PDF use it to generate a new PDF with only one page
+        This function takes a source PDF and uses it to generate a new PDF with only one page
         that is equal in length to the number of pages in the source PDF. This will make it easier
         to reformat the entire PDF after deleting unnecessary bullet points
         """
+
         source_pdf = pymupdf.open(path_to_source_pdf)
         self.source_pdf_details = {
             "page_count": source_pdf.page_count,
@@ -90,8 +81,9 @@ class PdfDocument(Document):
             "height": source_pdf[0].rect.height
         }
 
-        # TODO account for header and footer on each page
-        # TODO do this on resume upload
+        if not all(list(self.source_pdf_details.values())):
+            # TODO Error Handling
+            raise KeyError
 
         # creates unified pdf with one page that is as long as there are pages in our source pdf
         source_pdf_unified = pymupdf.open()
@@ -100,36 +92,46 @@ class PdfDocument(Document):
             height=self.source_pdf_details["height"] * self.source_pdf_details["page_count"]
         )
 
-        # Adds source pages onto unified pdf
+        header_from_this_page = None
+        footer_from_previous_page = None
+
+        # Maps source pages onto unified pdf
         for page in source_pdf:
             # determines how many pages we've already added/need to account for
             page_offset_height = self.source_pdf_details["height"] * page.number
 
+            updated_top_of_page = page.rect.y0 + page_offset_height
+            updated_bottom_of_page = page.rect.y1 + page_offset_height
+
             location_on_unified_pdf = pymupdf.Rect(
                 page.rect.x0,
-                page.rect.y0 + page_offset_height,
+                updated_top_of_page,
                 page.rect.x1,
-                page.rect.y1 + page_offset_height)
+                updated_bottom_of_page)
 
             source_pdf_page_unified.show_pdf_page(location_on_unified_pdf, source_pdf, page.number)
 
-        unified_tailored_pdf = pymupdf.open()
-        unified_tailored_pdf.new_page(
-            width=self.source_pdf_details["width"],
-            height=self.source_pdf_details["height"] * self.source_pdf_details["page_count"]
-        )
+            # calculates page break spacing on unified pdf (if applicable)
+            header_height, footer_height = self.calculate_page_break_spacing(page)
+            header_from_this_page = updated_top_of_page + header_height
 
-        return source_pdf_unified, unified_tailored_pdf
+            # checks if we already have a footer from the previous page and can calculate the combined volume
+            if footer_from_previous_page and footer_from_previous_page < header_from_this_page:
+                rect = pymupdf.Rect(0, footer_from_previous_page, self.source_pdf_details["width"], header_from_this_page)
+                self.page_break_rects.append(rect)
+
+            footer_from_previous_page = updated_bottom_of_page - footer_height
+
+        return source_pdf_unified
 
     def split_unified_pdf(self, unified_pdf):
         """
         Splits our unified PDF back into the original PDF length
         """
         # unpacks source details dict values and checks if all are defined
-        # TODO make sure no values can be 0 or empty
         if not all(list(self.source_pdf_details.values())):
             # TODO Error Handling
-            return
+            raise Exception
 
         tailored_resume = pymupdf.open()
 
@@ -157,10 +159,11 @@ class PdfDocument(Document):
 
         return tailored_resume
 
-    def redact_bullets_from_page(self, bullets_to_redact: [], source_page: pymupdf.Page):
+    def redact_bullets_from_pdf(self, bullets_to_redact: [], source_pdf: pymupdf.Document):
         # TODO Define default bullet format
         # Note bullets and newline commands not included in blocks, can be left over from deletion,
         # need to read entire line and redact extra characters in order to make sure text block doesn't pick up
+        source_page = source_pdf[0]
 
         for bullet in bullets_to_redact:
             bullet_rect = None
@@ -180,22 +183,32 @@ class PdfDocument(Document):
             # TODO error handling
             print("could not find any bullets on the page")
 
-        return source_page
+        return source_pdf
 
-    def format_tailored_pdf_unified(self, redacted_page: pymupdf.Page, tailored_page_unified: pymupdf.Page, source_pdf_unified: pymupdf.Document):
+    def format_tailored_pdf_unified(self, redacted_pdf: pymupdf.Document):
+        redacted_page = redacted_pdf[0]
+
+        tailored_pdf_unified = pymupdf.open()
+        tailored_pdf_unified.new_page(
+            width=self.source_pdf_details["width"],
+            height=self.source_pdf_details["height"] * self.source_pdf_details["page_count"]
+        )
+
+        tailored_page_unified = tailored_pdf_unified[0]
+
         redacted_index = 0
         offset_by = 0
 
         for text_block in redacted_page.get_text("blocks"):
-            print(text_block)
+            # print(text_block)
             source_rect = self._get_rect(text_block)
 
             # handle when nothing to redact or we've redacted everything
             if self.redacted_rects and redacted_index < len(self.redacted_rects):
                 redacted_rect = self.redacted_rects[redacted_index]
                 if source_rect.y0 > redacted_rect.y1:
-                    print("we found a moment where our block is lower")
-                    print(text_block)
+                    # print("we found a moment where our block is lower")
+                    # print(text_block)
 
                     # calculate how much we need to offset by
                     redacted_offset = redacted_rect.height
@@ -207,16 +220,55 @@ class PdfDocument(Document):
                     # print(current_redaction.height)
                     # print(f"new offset is {offset_by}")
 
-            # TODO handle header footer offset
-            updated_rect = self._get_rect(text_block, offset_by)
+            updated_rect = self.calculate_updated_rect(text_block, offset_by)
+
             # print(f"source rect is {source_rect}")
             # print(f"updated rect is {updated_rect}")
             tailored_page_unified.show_pdf_page(
                 updated_rect,
-                source_pdf_unified,
+                redacted_pdf,
                 redacted_page.number,
                 clip=source_rect
             )
+
+        return tailored_pdf_unified
+
+    def calculate_updated_rect(self, text_block, offset_by):
+        updated_rect = self._get_rect(text_block, offset_by)
+
+        # check if updated rectangle intersects with a page break
+        print("checking page break rects")
+        for page_break_rect in self.page_break_rects:
+            if page_break_rect.intersects(updated_rect):
+                # TODO break up only part of text block that overlaps
+                updated_offset = offset_by + page_break_rect.height
+                updated_rect = self._get_rect(text_block, updated_offset)
+
+        return updated_rect
+
+
+    @staticmethod
+    def calculate_page_break_spacing(page: pymupdf.Page):
+        """
+        returns the distance (in pixels) of the text closest to the top of the page (bottom of header)
+        and the bottom of the page (top of footer)
+        """
+
+        # Note: get_text("blocks") returns array of tuples in the form:
+        # (x0, y0, x1, y1, "lines in the block", block_no, block_type)
+
+        # distance from top of the page to the text block with the lowest y0 value (bottom of header)
+        top_of_page = 0
+        bottom of header = min(text_block[1] for text_block in page.get_text("blocks"))
+        header_height = top_of_page + bottom of header
+
+        # distance from the bottom of the page to the text block with the largest y1 value (top of footer)
+        bottom_of_page = page.rect.height
+        top of footer = max(text_block[3] for text_block in page.get_text("blocks"))
+        footer_height = bottom_of_page - top of footer
+
+        return header_height, footer_height
+
 
     @staticmethod
     def _combine_rects(existing_rect: pymupdf.Rect, new_rect: pymupdf.Rect):
