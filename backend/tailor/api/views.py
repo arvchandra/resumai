e6515656f05743c5e0e3de79be6a5bf6aa75467b
Arvind Chandra <arvchandra@gmail.com>
2025-07-15 11:30:11 +0530

    More cleanup: remove unused management commands and API views and URLs, consolidated images into one folder, removed frontend mock data,

--- backend/tailor/api/views.py
+++ backend/tailor/api/views.py
@@ -4,8 +4,6 @@ from openai import OpenAI
 from rest_framework import status
 from rest_framework.exceptions import NotFound, ValidationError
 from rest_framework.generics import ListAPIView
-from rest_framework.permissions import AllowAny
-from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
--- backend/tailor/urls.py
+++ backend/tailor/urls.py
@@ -1,17 +1,11 @@
 from django.urls import path
 from tailor.api.views import (
     UserResumeUploadView, 
-    ParseJobPostingView, 
     TailorResumeView,
     UserResumeListView
 )
 
 urlpatterns = [
-    path(
-        "jobposting/",
-        ParseJobPostingView.as_view(),
-        name="job_posting_text",
-    ),
     path(
         "users/<int:user_id>/resumes/",
         UserResumeListView.as_view(),
