7b7f2b78aa734495941aa5a2323c9815a5e3c537
Arvind Chandra <arvchandra@gmail.com>
2025-07-22 17:53:58 +0530

    Linting changes

--- frontend/src/components/TailoredResumeTable/TailoredResumeTable.tsx
+++ frontend/src/components/TailoredResumeTable/TailoredResumeTable.tsx
@@ -4,10 +4,10 @@ import useFetchWithAuth from "../../hooks/useFetchWithAuth";
 import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
 import type { CustomCellRendererProps } from 'ag-grid-react';
 import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
-import type { 
+import type {
   ICellRendererParams,
   SizeColumnsToFitGridStrategy,
-  SizeColumnsToFitProvidedWidthStrategy, 
+  SizeColumnsToFitProvidedWidthStrategy,
   SizeColumnsToContentStrategy
 } from 'ag-grid-community';
 import fileDownloadIcon from "../../assets/images/download-file-icon.png";
@@ -20,7 +20,7 @@ type TailoredResumeResponse = {
   company: string;
   role: string;
   job_posting: string;
-  created_at: string; 
+  created_at: string;
 }
 
 type RowData = {
@@ -37,14 +37,14 @@ const colDefs: ColDef[] = [
   { "field": 'name' },
   { "field": 'company' },
   { "field": 'role' },
-  { 
+  {
     "field": 'job_posting',
     "headerName": "Job",
     "cellRenderer": jobPostingCellRenderer,
   },
   { "field": 'created_at' },
-  { 
-    "field": 'download' ,
+  {
+    "field": 'download',
     "cellRenderer": downloadCellRenderer,
     "flex": 1
   },
@@ -56,31 +56,31 @@ export default function TailoredResumeTable() {
   const [rowData, setRowData] = useState<RowData[]>([]);
 
   const autoSizeStrategy = useMemo<
-    | SizeColumnsToFitGridStrategy 
+    | SizeColumnsToFitGridStrategy
     | SizeColumnsToFitProvidedWidthStrategy
     | SizeColumnsToContentStrategy
-    >(() => ({
-      type: "fitCellContents",
-      defaultMaxWidth: 100,
-    }), [],
+  >(() => ({
+    type: "fitCellContents",
+    defaultMaxWidth: 100,
+  }), [],
   );
 
   useEffect(() => {
-    const fetchTailoredResumeData= async () => {
+    const fetchTailoredResumeData = async () => {
       try {
-        const response  = await fetchWithAuth("http://localhost:8000/tailor/users/2/tailored-resumes");
+        const response = await fetchWithAuth("http://localhost:8000/tailor/users/2/tailored-resumes");
         const tailoredResumeData = await response.json();
-  
+
         if (!response.ok) {
-            throw new Error("Failed to fetch tailored resumes");
+          throw new Error("Failed to fetch tailored resumes");
         }
 
         if (!Array.isArray(tailoredResumeData)) {
           throw new Error("Tailored resume response improperly formatted");
         }
 
-        const formattedRows = formatTailoredResumesToAgGridRows(tailoredResumeData, colDefs );
-        
+        const formattedRows = formatTailoredResumesToAgGridRows(tailoredResumeData, colDefs);
+
         // TODO switch from typecasting once we have landed on a final table format
         if (formattedRows) {
           setRowData(formattedRows as RowData[]);
@@ -92,23 +92,26 @@ export default function TailoredResumeTable() {
 
     fetchTailoredResumeData();
   }, []);
-  
+
 
   return (
     <div className="form-field">
-        <label>Tailored Resumes:</label>
-        <div className="grid" style={{ height: 200 }}>
-          <AgGridReact
-              rowData={rowData}
-              columnDefs={colDefs}
-              autoSizeStrategy={autoSizeStrategy}
-          />
-        </div>
+      <label>Tailored Resumes:</label>
+      <div className="grid" style={{ height: 200 }}>
+        <AgGridReact
+          rowData={rowData}
+          columnDefs={colDefs}
+          autoSizeStrategy={autoSizeStrategy}
+        />
+      </div>
     </div>
   );
 };
 
-function formatTailoredResumesToAgGridRows( tailoredResumeData: TailoredResumeResponse[], columnDefs: ColDef[] ) {
+function formatTailoredResumesToAgGridRows(
+  tailoredResumeData: TailoredResumeResponse[], 
+  columnDefs: ColDef[]
+) {
   // retrieve our "field" key from each column in our Ag-Grid column definitions (e.g. "field": "company")
   const columnLabels = columnDefs.map((column) => column.field);
 
@@ -118,14 +121,14 @@ function formatTailoredResumesToAgGridRows( tailoredResumeData: TailoredResumeRe
     Object.fromEntries(
       columnLabels
         .filter(key => key in resume)
-        .map(key => [key, resume[key as keyof TailoredResumeResponse] ])
+        .map(key => [key, resume[key as keyof TailoredResumeResponse]])
     )
   );
-  
+
   return formattedTailoredResumeRows
 };
 
-function jobPostingCellRenderer({ data }: CustomCellRendererProps){
+function jobPostingCellRenderer({ data }: CustomCellRendererProps) {
   return (
     <a href={data.job_posting} target="_blank" rel="noopener noreferrer">
       Link
@@ -133,8 +136,15 @@ function jobPostingCellRenderer({ data }: CustomCellRendererProps){
   );
 };
 
-function downloadCellRenderer({ data }: ICellRendererParams){
+function downloadCellRenderer({ data }: ICellRendererParams) {
   return (
-    <img className="action-icon" src={fileDownloadIcon} width={15} height={15} alt="Download" onClick={() => console.log("file downloaded")} />
+    <img
+      className="action-icon"
+      src={fileDownloadIcon} 
+      width={15} 
+      height={15} 
+      alt="Download" 
+      onClick={() => console.log("file downloaded")} 
+    />
   );
 };
