e1feeb70719d4b6ed237b81297b7c27a1197f383
Max Ingraham-Rakatansky <maxmir@berkeley.edu>
2025-08-12 20:11:27 +0100

    function ordering

--- backend/tailor/domain/tailor_resume.py
+++ backend/tailor/domain/tailor_resume.py
@@ -62,7 +62,7 @@ class TailorPdf:
         }
 
         # creates unified pdf with one page that is as long as there are pages in our template pdf
-        template_pdf_unified = self._generate_unified_pdf(*self.template_pdf_details.values())
+        template_pdf_unified = self._generate_unified_pdf()
         template_pdf_page_unified = template_pdf_unified[0]
 
         # Maps template pages onto unified pdf
@@ -130,6 +130,10 @@ class TailorPdf:
         return page_break_rects
 
     def redact_bullets_from_pdf(self, bullets_to_redact: [], template_pdf: pymupdf.Document):
+        """
+        Identifies where in the page our bullets are that we want to delete, saves the location of where they were
+        as a Rect (including the line break spacing between them and the next bullet point), and then redacts them
+        """
         template_page = template_pdf[0]
 
         for bullet in bullets_to_redact:
@@ -143,6 +147,7 @@ class TailorPdf:
             self.redacted_rects.append(redacted_rect)
             template_page.add_redact_annot(redacted_rect)
 
+        # TODO remove once OpenAI Response is correctly sorting the bullet points
         self.redacted_rects = sorted(self.redacted_rects, key=lambda rect: rect.y0)
 
         result = template_page.apply_redactions()
@@ -185,28 +190,11 @@ class TailorPdf:
     def format_tailored_pdf_unified(self, redacted_pdf: pymupdf.Document):
         """
         This function repositions the remaining text on our redacted pdf onto a new pdf using the location of our
-        redacted rects to increase add how much we are moving the repositioned text up by
-
-        Example:
-        Template PDF
-        - A
-        - B
-        - C
-        - D
-
-        Redacted PDF
-        - A
-             <- B has been redacted
-             <- C has been redacted
-        - D  <- needs to be repositioned on the new pdf by the height of B + C
-
-        Tailored PDF
-        - A
-        - D
+        redacted rects to determine how much we are moving the repositioned text up by.
         """
         redacted_page = redacted_pdf[0]
 
-        tailored_pdf_unified = self._generate_unified_pdf(*self.template_pdf_details.values())
+        tailored_pdf_unified = self._generate_unified_pdf()
         tailored_page_unified = tailored_pdf_unified[0]
 
         redacted_rect_index = 0
@@ -233,9 +221,29 @@ class TailorPdf:
 
     def calculate_text_rect_offset(self, redacted_rect_index: int, text_block_rect: pymupdf.Rect):
         """
-        This function is meant to use our self.page_break_rects to reposition the rect passed in
-        if it falls into one of the rects that capture our footer + header spacing. If it does
-        intersect, then we will split/reposition the text_block so it accommodates the page break spacing
+        Here we calculate if the text_block_rect we are evaluating is below the redacted_rect located at our index.
+        If it is, then text_block_rect is the next available text_block after our redactions, which means that we could
+        have jumped over multiple redactions to get there.
+
+        Example:
+        Template PDF
+        - A
+        - B
+        - C
+        - D
+
+        Redacted PDF
+        - A
+             <- B has been redacted
+             <- C has been redacted
+        - D  <- needs to be repositioned on the new pdf by the height of B + C
+
+        Tailored PDF
+        - A
+        - D
+
+        This means we need to keep stepping along our redacted_rect list until our text_block_rect is no longer
+        below a redacted_rect or we have run through all of our redacted_rects
         """
         # handle when nothing to redact or we've reached the end of our list
         if not self.redacted_rects or redacted_rect_index >= len(self.redacted_rects):
@@ -258,6 +266,66 @@ class TailorPdf:
                 break
 
         return redacted_offset, current_redacted_rect_index
+
+    def isolate_repositioned_rect(self, repositioned_rect, redacted_pdf, template_rect):
+        """
+        This function takes our repositioned text block from the template resume and copies that Rect onto a
+        temporary PDF. Because pymupdf's show_pdf_page will also copy all text, links, and annotations on the page,
+        not just the text inside the Rect (See: https://pymupdf.readthedocs.io/en/latest/page.html#Page.show_pdf_page)
+        we need to annotate all the space around our repositioned text block and redact it to remove all extraneous
+        text/links. We then return the temporary PDF page with only the text block.
+        """
+        interim_pdf_unified = self._generate_unified_pdf()
+        interim_page_unified = interim_pdf_unified[0]
+
+        interim_page_unified.show_pdf_page(
+            repositioned_rect,
+            redacted_pdf,
+            clip=template_rect
+        )
+
+        page_rect = interim_page_unified.rect
+
+        # redact everything else by creating four Rects around our rect and redacting everything around it
+        rect_above_repositioned_rect = self._get_rect([
+            page_rect.x0,  # Leftmost part of Page
+            page_rect.y0,  # Top of Page
+            page_rect.x1,  # Rightmost part of Page
+            repositioned_rect.y0  # Top of repositioned rect
+        ])
+
+        rect_left_of_repositioned_rect = self._get_rect([
+            page_rect.x0,  # Leftmost part of Page
+            repositioned_rect.y0,  # Top of repositioned rect
+            repositioned_rect.x0,  # Leftmost part of repositioned rect
+            repositioned_rect.y1  # Bottom of repositioned rect
+        ])
+
+        rect_right_of_repositioned_rect = self._get_rect([
+            repositioned_rect.x1,  # Rightmost part of repositioned rect
+            repositioned_rect.y0,  # Top of repositioned rect
+            page_rect.x1,  # Rightmost part of Page
+            repositioned_rect.y1  # Bottom of repositioned rect
+        ])
+
+        rect_below_repositioned_rect = self._get_rect([
+            page_rect.x0,  # Leftmost part of Page
+            repositioned_rect.y1,  # Bottom of repositioned rect
+            page_rect.x1,  # Rightmost part of Page
+            page_rect.y1  # Bottom of page
+        ])
+
+        for redact_rect in (
+                rect_above_repositioned_rect,
+                rect_left_of_repositioned_rect,
+                rect_right_of_repositioned_rect,
+                rect_below_repositioned_rect
+        ):
+            interim_page_unified.add_redact_annot(redact_rect)
+
+        interim_page_unified.apply_redactions()
+        interim_pdf_unified.reload_page(interim_page_unified)
+        return interim_pdf_unified
 
     def split_unified_pdf(self, unified_pdf):
         """
@@ -323,66 +391,6 @@ class TailorPdf:
 
         return bullet_rect
 
-    def isolate_repositioned_rect(self, repositioned_rect, redacted_pdf, template_rect):
-        """
-        This function takes our repositioned text block from the template resume and copies that Rect onto a
-        temporary PDF. Because pymupdf's show_pdf_page will also copy all text, links, and annotations on the page,
-        not just the text inside the Rect (See: https://pymupdf.readthedocs.io/en/latest/page.html#Page.show_pdf_page)
-        we need to annotate all the space around our repositioned text block and redact it to remove all extraneous
-        text/links. We then return the temporary PDF page with only the text block.
-        """
-        interim_pdf_unified = self._generate_unified_pdf(*self.template_pdf_details.values())
-        interim_page_unified = interim_pdf_unified[0]
-
-        interim_page_unified.show_pdf_page(
-            repositioned_rect,
-            redacted_pdf,
-            clip=template_rect
-        )
-
-        page_rect = interim_page_unified.rect
-
-        # redact everything else by creating four Rects around our rect and redacting everything around it
-        rect_above_repositioned_rect = self._get_rect([
-            page_rect.x0,  # Leftmost part of Page
-            page_rect.y0,  # Top of Page
-            page_rect.x1,  # Rightmost part of Page
-            repositioned_rect.y0  # Top of repositioned rect
-        ])
-
-        rect_left_of_repositioned_rect = self._get_rect([
-            page_rect.x0,  # Leftmost part of Page
-            repositioned_rect.y0,  # Top of repositioned rect
-            repositioned_rect.x0,  # Leftmost part of repositioned rect
-            repositioned_rect.y1  # Bottom of repositioned rect
-        ])
-
-        rect_right_of_repositioned_rect = self._get_rect([
-            repositioned_rect.x1,  # Rightmost part of repositioned rect
-            repositioned_rect.y0,  # Top of repositioned rect
-            page_rect.x1,  # Rightmost part of Page
-            repositioned_rect.y1  # Bottom of repositioned rect
-        ])
-
-        rect_below_repositioned_rect = self._get_rect([
-            page_rect.x0,  # Leftmost part of Page
-            repositioned_rect.y1,  # Bottom of repositioned rect
-            page_rect.x1,  # Rightmost part of Page
-            page_rect.y1  # Bottom of page
-        ])
-
-        for redact_rect in (
-                rect_above_repositioned_rect,
-                rect_left_of_repositioned_rect,
-                rect_right_of_repositioned_rect,
-                rect_below_repositioned_rect
-        ):
-            interim_page_unified.add_redact_annot(redact_rect)
-
-        interim_page_unified.apply_redactions()
-        interim_pdf_unified.reload_page(interim_page_unified)
-        return interim_pdf_unified
-
     def _combine_rects(self, rect_list=[]):
         if not rect_list:
             return None
@@ -409,15 +417,15 @@ class TailorPdf:
         combined_rect = self.extend_borders_to_width(combined_rect)
         return combined_rect
 
+    def _generate_unified_pdf(self):
+        page_count, page_width, page_height = self.template_pdf_details.values()
+        new_pdf = pymupdf.open()
+        new_pdf.new_page(width=page_width, height=page_height * page_count)
+        return new_pdf
+
     @staticmethod
     def _get_rect(block, offset=0):
         rect = pymupdf.Rect(block[0], block[1] - offset, block[2], block[3] - offset)
         if not rect.is_valid or rect.is_empty:
             raise KeyError("generated_rect is invalid")
         return rect
-
-    @staticmethod
-    def _generate_unified_pdf(page_count, page_width, page_height):
-        new_pdf = pymupdf.open()
-        new_pdf.new_page(width=page_width, height=page_height * page_count)
-        return new_pdf
