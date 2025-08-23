5dce169b4185b793d71332ff9cb5b49c6e1a8082
Arvind Chandra <arvchandra@gmail.com>
2025-08-23 15:29:24 +0530

    Initial commit

---
+++ frontend/browser_extension/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import "../src/assets/styles/shared.css";
+
+function App() {
+  return (
+    <div>
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, "browser_extension/popup.html"),          // extension popup
+        background: resolve(__dirname, "browser_extension/background.ts"),  // background worker
+      },
+    },
+    outDir: "dist",
+    emptyOutDir: true
+  },
+
   server: {
     port: 3000,
   },
